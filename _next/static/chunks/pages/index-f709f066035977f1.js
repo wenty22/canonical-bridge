af734816bcd21e63540272ff661c0ceeee01b86d
wenty22 <github-actions[bot]@users.noreply.github.com>
2024-10-22 07:36:49 +0000

    Update from https://github.com/bnb-chain/canonical-bridge/commit/1789ead6d49c758f731cc4d88efba8c2716777c2

---
+++ _next/static/chunks/pages/index-f709f066035977f1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{46751:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return _(89902)}])},89902:function(n,u,_){"use strict";_.r(u);var t=_(34728);u.default=t.default}},function(n){n.O(0,[471,157,809,728,888,774,179],function(){return n(n.s=46751)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/pages/mainnet-ddcfe3e409fce7a5.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[562],{73344:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/mainnet",function(){return u(34728)}])}},function(n){n.O(0,[471,157,809,728,888,774,179],function(){return n(n.s=73344)}),_N_E=n.O()}]);
