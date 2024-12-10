4d62e374a3228ff52b160fd440f390b9a2b0dddc
wenty22 <github-actions[bot]@users.noreply.github.com>
2024-12-10 02:45:20 +0000

    Update from https://github.com/bnb-chain/canonical-bridge/commit/5aa42892fc64d9338216b028887c5a1c50176ea1

--- _next/static/chunks/pages/mainnet-05545a586684b0bb.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[562],{44575:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/mainnet",function(){return u(96534)}])}},function(n){n.O(0,[636,64,23,92,157,52,534,888,774,179],function(){return n(n.s=44575)}),_N_E=n.O()}]);
